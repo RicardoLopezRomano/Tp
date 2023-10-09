0d12263cfefda400d3ea2fd1b8d13ec572a875f7
Yesica M <yesik2607m@gmail.com>
2023-10-08 23:21:20 -0300

    cambios, hay cosas q aun no me salen

---
+++ TP/js/proximosEstrenos.js
@@ -0,0 +1,10 @@
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const menu = document.querySelector(".menu");
+
+    menuIcon.addEventListener("click", function () {
+        menu.classList.toggle("show");
+    });
+});
+*/
